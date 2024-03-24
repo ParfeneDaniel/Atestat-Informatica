6cb1a2253154a8249c9b6e05f2b5d10842712417
Parfene Daniel <160225733+ParfeneDaniel@users.noreply.github.com>
2024-03-24 17:33:44 +0200

    apply prettier

--- api/utils/generateToken.js
+++ api/utils/generateToken.js
@@ -3,15 +3,15 @@ import jwt from "jsonwebtoken";
 const JWT_SECRET = process.env.JWT_SECRET;
 
 export const generateTokenAndSetCookie = (id, res) => {
-    const token = jwt.sign({ id }, JWT_SECRET, {
-        "expiresIn": "1d",
-        //expiresIn: "30s",
-    });
-    res.cookie("jwt", token, {
-       maxAge: 1 * 24 * 60 * 60 * 1000,
-       //maxAge: 30 * 1000,
-       httpOnly: true,
-       sameSite: "strict",
-       secure: true,
-    });
-}
+  const token = jwt.sign({ id }, JWT_SECRET, {
+    expiresIn: "1d",
+    //expiresIn: "30s",
+  });
+  res.cookie("jwt", token, {
+    maxAge: 1 * 24 * 60 * 60 * 1000,
+    //maxAge: 30 * 1000,
+    httpOnly: true,
+    sameSite: "strict",
+    secure: true,
+  });
+};
